a4d7205b50b762748ff1913e5cd757c9281122b8
AnneliesVlaar <66774020+AnneliesVlaar@users.noreply.github.com>
2022-09-12 14:17:14 +0200

    Initial commit

---
+++ src/smallangle/smallangle.py
@@ -0,0 +1,22 @@
+import click
+import numpy as np
+from numpy import pi
+import pandas as pd
+
+
+def sin(number):
+    x = np.linspace(0, 2 * pi, number)
+    df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
+    print(df)
+    return
+
+
+def tan(number):
+    x = np.linspace(0, 2 * pi, number)
+    df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
+    print(df)
+    return
+
+
+if __name__ == "__main__":
+    sin(10)
