9eee28bd72a057e5fea251a652e3a8f418a38c82
Annelies Vlaar <66774020+AnneliesVlaar@users.noreply.github.com>
2024-11-07 16:33:59 +0100

    remove unused return

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -8,14 +8,12 @@ def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
-    return
 
 
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
-    return
 
 
 if __name__ == "__main__":
